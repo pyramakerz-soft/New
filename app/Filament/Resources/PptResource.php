da74f83cff26205ddf0d841e777a6bf11e69cda0
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2024-07-16 13:57:27 +0300

    edit in ppt

--- app/Filament/Resources/PptResource.php
+++ app/Filament/Resources/PptResource.php
@@ -24,7 +24,6 @@ use Illuminate\Support\Facades\DB;
 class PPTResource extends Resource
 {
     protected static ?string $model = PPT::class;
-    protected static ?string $navigationGroup = 'Categories';
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
