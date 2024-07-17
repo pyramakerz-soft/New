ebf846f3af2f2f48e93c41b33d2b8cb849355fa7
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2024-07-17 15:01:56 +0300

    edit lesson controller to get game by number

--- app/Http/Controllers/Api/LessonController.php
+++ app/Http/Controllers/Api/LessonController.php
@@ -16,88 +16,93 @@ class LessonController extends Controller
     use HelpersTrait;
 
     /**
- * @OA\Get(
- *     path="/api/lessons/{id}",
- *     summary="Get lessons by unit ID",
- *     tags={"Lesson"},
- *     security={{"bearerAuth":{}}},
- *     @OA\Parameter(
- *         name="id",
- *         in="path",
- *         description="Unit ID",
- *         required=true,
- *         @OA\Schema(type="integer")
- *     ),
- *     @OA\Response(
- *         response=200,
- *         description="Lessons fetched successfully",
- *         @OA\JsonContent(
- *             @OA\Property(property="data", type="object"),
- *             @OA\Property(property="message", type="string")
- *         )
- *     )
- * )
- */
+     * @OA\Get(
+     *     path="/api/lessons/{id}",
+     *     summary="Get lessons by unit ID",
+     *     tags={"Lesson"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Unit ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Lessons fetched successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object"),
+     *             @OA\Property(property="message", type="string")
+     *         )
+     *     )
+     * )
+     */
     public function index($id)
     {
         // $units = Unit::all();
         $data['lessons'] = Lesson::with(['game', 'game.gameTypes'])->where("unit_id", $id)->orderBy('number')->get();
+        $data['lessons']->each(function ($lesson) {
+            $lesson->game = $lesson->game->sortBy('number');
+        });
         $resource = LessonResource::make($data['lessons']);
         $data['types'] = GameType::all();
         return $this->returnData('data', $resource, "All lessons");
     }
 
     /**
- * @OA\Get(
- *     path="/api/lesson_questions/{id}",
- *     summary="Get questions of a lesson",
- *     tags={"Lesson"},
- *     security={{"bearerAuth":{}}},
- *     @OA\Parameter(
- *         name="id",
- *         in="path",
- *         description="Lesson ID",
- *         required=true,
- *         @OA\Schema(type="integer")
- *     ),
- *     @OA\Response(
- *         response=200,
- *         description="Questions fetched successfully",
- *         @OA\JsonContent(
- *             @OA\Property(property="data", type="object"),
- *             @OA\Property(property="message", type="string")
- *         )
- *     )
- * )
- */
-    public function lessonQuestions($id){
-        $data['questions'] = Question::join('lessons','questions.lesson_id','lessons.id')->where('lessons.id',$id)->select('questions.*')->get();
+     * @OA\Get(
+     *     path="/api/lesson_questions/{id}",
+     *     summary="Get questions of a lesson",
+     *     tags={"Lesson"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Lesson ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Questions fetched successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object"),
+     *             @OA\Property(property="message", type="string")
+     *         )
+     *     )
+     * )
+     */
+    public function lessonQuestions($id)
+    {
+        $data['questions'] = Question::join('lessons', 'questions.lesson_id', 'lessons.id')->where('lessons.id', $id)->select('questions.*')->get();
         return $this->returnData('data', $data, "All questions");
     }
-/**
- * @OA\Get(
- *     path="/api/lesson/get-complete-status/{id}",
- *     summary="Get complete status of a lesson",
- *     tags={"Lesson"},
- *     security={{"bearerAuth":{}}},
- *     @OA\Parameter(
- *         name="id",
- *         in="path",
- *         description="Lesson ID",
- *         required=true,
- *         @OA\Schema(type="integer")
- *     ),
- *     @OA\Response(
- *         response=200,
- *         description="Lesson status fetched successfully",
- *         @OA\JsonContent(
- *             @OA\Property(property="data", type="object"),
- *             @OA\Property(property="message", type="string")
- *         )
- *     )
- * )
- */
-    public function getCompleteStatus($id){
+    /**
+     * @OA\Get(
+     *     path="/api/lesson/get-complete-status/{id}",
+     *     summary="Get complete status of a lesson",
+     *     tags={"Lesson"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Lesson ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Lesson status fetched successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object"),
+     *             @OA\Property(property="message", type="string")
+     *         )
+     *     )
+     * )
+     */
+    public function getCompleteStatus($id)
+    {
         $data['status'] = Lesson::find($id)->status;
         return $this->returnData('data', $data, "Lesson Status");
     }
